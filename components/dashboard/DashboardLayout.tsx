929b88243bec947172866cc1b4c6aa302651fc00
Michael <237919117+wonderingtribe@users.noreply.github.com>
2025-11-10 20:58:05 -0600

    Update DashboardLayout.tsx

--- components/dashboard/DashboardLayout.tsx
+++ components/dashboard/DashboardLayout.tsx
@@ -11,21 +11,13 @@ interface DashboardLayoutProps {
 
 export const DashboardLayout: React.FC<DashboardLayoutProps> = ({ children }) => {
   return (
-    <div className="wonderland-bg min-h-screen flex">
-      {/* Sidebar */}
+    <div className="wonderland-bg min-h-screen">
+      <TopNavigation />
       <Sidebar />
-
-      {/* Main content wrapper */}
-      <div className="flex-1 flex flex-col min-h-screen md:pl-64 pt-24 md:pt-28 pb-24 md:pb-8 px-4 md:px-8 overflow-auto">
-        {/* Top Navigation */}
-        <TopNavigation />
-
-        {/* Page Content */}
-        <main className="flex-1">{children}</main>
-
-        {/* Bottom Navigation */}
-        <BottomNavigation />
-      </div>
+      <main className="md:ml-64 pt-24 md:pt-28 pb-24 md:pb-8 px-4 md:px-8">
+        {children}
+      </main>
+      <BottomNavigation />
     </div>
   );
 };
